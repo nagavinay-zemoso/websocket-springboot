ef8dcffcc59e518d93f77e88ed5f25bea9610cf2
Nagavinay Chinthada <nagavinay.chinthada@zemosolabs.com>
2023-05-12 10:54:55 +0530

    websocket in springboot

---
+++ src/main/java/com/zemoso/websocket/springbootwebsocket/HelloMessage.java
@@ -0,0 +1,13 @@
+package com.zemoso.websocket.springbootwebsocket;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class HelloMessage {
+
+    private String name;
+}
